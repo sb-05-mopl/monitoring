328efeeca002a70500873423d9c9af4e91846ebb
jeonggiju <halogiju123@gmail.com>
2025-12-24 15:36:08 +0900

    spring init

--- src/main/java/com/mopl/monitor/MonitorApplication.java
+++ src/main/java/com/mopl/monitor/MonitorApplication.java
@@ -3,10 +3,11 @@ package com.mopl.monitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MonitorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MonitorApplication.class, args);
 	}
-
+///
 }
